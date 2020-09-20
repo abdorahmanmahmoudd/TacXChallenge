705b4ecfe4c1fd6030c271a04fe981a1e6eb1c49
Abdelrahman Ali <abdelrahman.ali@rtl.nl>
2020-09-20 14:05:56 +0200

    Initial Commit

---
+++ TacXApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TacXApp
+//
+//  Created by Abdelrahman Ali on 20/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
